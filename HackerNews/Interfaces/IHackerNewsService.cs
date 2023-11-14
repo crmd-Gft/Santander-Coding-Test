ac554a8b56b8e4b480cee735775c2ad6f8be0145
chris.mcleod <chris.mcleod@gft.com>
2023-11-14 23:18:32 +0000

    make calls sequential, use IHttpClientFactory

---
+++ HackerNews/Interfaces/IHackerNewsService.cs
@@ -0,0 +1,9 @@
+﻿namespace HackerNews
+{
+	public interface IHackerNewsService
+	{
+        public Task<IEnumerable<int>> GetBestStoryIdsAsync(int n);
+        public Task<Story> GetStoryDetailsAsync(int storyId);
+    }
+}
+
